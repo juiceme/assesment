8d33e074fbd9ce517b2a55bc0c6ae33dae7eb8a9
Jussi Ohenoja <juice@swagman.org>
2020-02-19 18:54:15 +0200

    rotation of even images works

--- rotate.py
+++ rotate.py
@@ -11,10 +11,33 @@ class RotateImage:
         if len(input_image) is not len(input_image[0]):
             print("Input image needs to be N*N square array")
             return False
-        print("OK")
         
+        # Check whether the array is even or odd, this is needed to determine
+        # if the center pixel need to be rotated or not
+        if len(input_image) % 2 == 0:
+            rounds = len(input_image)-1
+            for i in range(int(len(input_image)/2)):
+                for j in range(i, rounds-i):
+                    swap = input_image[i][j]
+                    input_image[i][j] = input_image[rounds-j][i]
+                    input_image[rounds-j][i] = input_image[rounds-i][rounds-j]
+                    input_image[rounds-i][rounds-j] = input_image[j][rounds-i]
+                    input_image[j][rounds-i] = swap
+            return input_image
+
+def printimage(image):
+    for x in range(len(image)):
+        for y in range(len(image)):
+            print(str(image[x][y]) + " ", end = '')
+        print()
+    print()
 
     
 a = RotateImage()
-a.rotate([[1, 2, 3],[4, 5, 6], [7, 8, 9]])
+image1 = [ [1, 2, 3], [4, 5, 6], [7, 8, 9] ]
+image2 = [ [1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16] ]
+#output1 = a.rotate(image1)
+printimage(image2)
+output2 = a.rotate(image2)
+printimage(output2)
         
